c7541a6a9f5a477ceb7fbe91b33a27d7ebabdc6d
Pylogmon <pylogmon@outlook.com>
2023-10-15 01:07:21 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -39,8 +39,10 @@ async fn do_work(text: &str) -> Result<Value, Box<dyn Error>> {
         let phonetic: String = row.try_get("phonetic")?;
         let translation: String = row.try_get("translation")?;
         let tag: String = row.try_get("tag")?;
+        let exchange: String = row.try_get("exchange")?;
         let translation_list = translation.split("\n").collect::<Vec<&str>>();
         let mut explanations: Vec<Value> = Vec::new();
+        let mut associations: Vec<String> = Vec::new();
 
         for line in translation_list {
             let temp = line.split(".").collect::<Vec<&str>>();
@@ -63,15 +65,33 @@ async fn do_work(text: &str) -> Result<Value, Box<dyn Error>> {
                 "explains": explain_list
             }));
         }
+        for item in exchange.split("/") {
+            let temp = item.split(":").collect::<Vec<&str>>();
+            let word = temp[1];
+            match temp[0] {
+                "p" => associations.push(format!("过去式: {word}")),
+                "d" => associations.push(format!("过去分词: {word}")),
+                "i" => associations.push(format!("现在分词: {word}")),
+                "3" => associations.push(format!("第三人称单数: {word}")),
+                "r" => associations.push(format!("比较级: {word}")),
+                "t" => associations.push(format!("最高级: {word}")),
+                "s" => associations.push(format!("复数: {word}")),
+                "0" => associations.push(format!("Lemma: {word}")),
+                "1" => associations.push(format!("Lemma: {word}")),
+                _ => {}
+            }
+        }
+        associations.push("".to_string());
+        associations.push(tag);
 
         return Ok(json!({
           "pronunciations": [
             {
-              "symbol": phonetic
+              "symbol": format!("/{phonetic}/")
             }
           ],
           "explanations": explanations,
-          "associations": tag.split(",").collect::<Vec<&str>>(),
+          "associations": associations
         }));
     }
     Err("Not found".into())
