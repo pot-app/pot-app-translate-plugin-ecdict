53ae2106b2f16429944c7a4599651f341a85e6f2
Pylogmon <pylogmon@outlook.com>
2023-10-14 23:56:44 +0800

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,88 @@
-use serde_json::Value;
+use dirs::config_dir;
+use futures::TryStreamExt;
+use serde_json::{json, Value};
+use sqlx::{Connection, Row, SqliteConnection};
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
+use tokio::runtime::Runtime;
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,
+    _from: &str,
+    _to: &str,
+    _detect: &str,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
-    let client = reqwest::blocking::ClientBuilder::new().build()?;
-
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => return Err("requestPath not found".into()),
-    };
-
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
-
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
-
-    let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
-        .send()?
-        .json()?;
-
-    fn parse_result(res: Value) -> Option<String> {
-        let result = res.as_object()?.get("translation")?.as_str()?.to_string();
-
-        Some(result.replace("@@", "/"))
-    }
-    if let Some(result) = parse_result(res) {
-        return Ok(Value::String(result));
-    } else {
-        return Err("Response Parse Error".into());
-    }
+    let rt = Runtime::new()?;
+    return Ok(rt.block_on(do_work(text))?);
 }
+async fn do_work(text: &str) -> Result<Value, Box<dyn Error>> {
+    let config_dir_path = config_dir().unwrap();
 
+    let db_path = config_dir_path
+        .join("com.pot-app.desktop")
+        .join("plugins")
+        .join("translate")
+        .join("[plugin].com.pot-app.ecdict")
+        .join("stardict.db");
+
+    let db_path = db_path.to_str().unwrap();
+
+    let mut conn = SqliteConnection::connect(&format!("sqlite:{db_path}")).await?;
+
+    let mut rows = sqlx::query("SELECT * FROM stardict WHERE word = ?")
+        .bind(text)
+        .fetch(&mut conn);
+
+    while let Some(row) = rows.try_next().await? {
+        let phonetic: String = row.try_get("phonetic")?;
+        let translation: String = row.try_get("translation")?;
+        let tag: String = row.try_get("tag")?;
+        let translation_list = translation.split("\n").collect::<Vec<&str>>();
+        let mut explanations: Vec<Value> = Vec::new();
+
+        for line in translation_list {
+            let temp = line.split(".").collect::<Vec<&str>>();
+            let mut trait_name = "";
+            let mut explains = Vec::new();
+
+            if temp.len() > 1 {
+                trait_name = temp[0];
+                explains = temp[1].split(",").collect::<Vec<&str>>();
+            } else {
+                trait_name = "";
+                explains = temp[0].split(",").collect::<Vec<&str>>();
+            }
+            let mut explain_list: Vec<Value> = Vec::new();
+            for explain in explains {
+                explain_list.push(Value::String(explain.to_string()));
+            }
+            explanations.push(json!({
+                "trait": trait_name,
+                "explains": explain_list
+            }));
+        }
+
+        return Ok(json!({
+          "pronunciations": [
+            {
+              "symbol": phonetic
+            }
+          ],
+          "explanations": explanations,
+          "associations": tag.split(",").collect::<Vec<&str>>(),
+        }));
+    }
+    Err("Not found".into())
+}
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", "zh_cn", needs).unwrap();
+        let needs = HashMap::new();
+        let result = translate("hello", "auto", "zh_cn", "zh_cn", needs).unwrap();
         println!("{result}");
     }
 }
